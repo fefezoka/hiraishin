220cd0186f595603c0def7aa883d2f20f25ec497
Ghigho <ghigho33@gmail.com>
2024-09-22 14:12:18 -0300

    feat: função para atualizar tabela

--- src/server/routers/sheets-fifa.ts
+++ src/server/routers/sheets-fifa.ts
@@ -2,6 +2,7 @@ import { GoogleSpreadsheet } from 'google-spreadsheet';
 import { JWT } from 'google-auth-library';
 import { procedure, router } from '../trpc';
 import { players } from '@/commons/fifa-data';
+import { z } from 'zod';
 
 export const fifaRouter = router({
   getSheetData: procedure.query(async () => {
@@ -50,4 +51,40 @@ export const fifaRouter = router({
 
     return formattedData;
   }),
+  updateSheetData: procedure
+  .input(
+    z.object
+    ({ 
+      value: z.string(),
+      column: z.number(),
+      row: z.number() 
+    }))
+    .query(async ({ input }) => {
+      const serviceAccountAuth = new JWT({
+        key: process.env.GOOGLE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+        email: process.env.GOOGLE_EMAIL,
+        scopes: [
+          'https://www.googleapis.com/auth/spreadsheets',
+          'https://www.googleapis.com/auth/drive.file',
+        ],
+      });
+
+      const doc = new GoogleSpreadsheet(
+        '1INObmp-0-mfdZCNTwyptcchLe6l7FKO572vr1BTM4Yg',
+        serviceAccountAuth
+      );
+
+      await doc.loadInfo();
+
+      const sheet = doc.sheetsByIndex[0];
+      
+
+      await sheet.loadCells('A1:I7');
+      const a1 = sheet.getCell(input.row, input.column);
+      a1.value = input.value;
+      await sheet.saveUpdatedCells();
+      return a1.value;
+    }),
 });
+
+
