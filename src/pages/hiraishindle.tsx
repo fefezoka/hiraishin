d01a9bad31cbcc3d11f5d774620b744072465d9a
Felipe Brito <89111098+fefezoka@users.noreply.github.com>
2025-03-04 18:20:13 -0300

    fix: check if localstorage answers has the chosen character

--- src/pages/hiraishindle.tsx
+++ src/pages/hiraishindle.tsx
@@ -42,6 +42,8 @@ export default function Hiraishindle() {
       today.getDay() !== lastAnswerDate.getDay() &&
       today.getMonth() !== lastAnswerDate.getMonth()
     ) {
+      localStorage.removeItem('hiraishindle-answers');
+      localStorage.removeItem('hiraishindle-last_answer_timestamp');
       return;
     }
 
@@ -49,7 +51,10 @@ export default function Hiraishindle() {
       | string[]
       | null;
 
-    answers && setAnswers(answers);
+    if (answers) {
+      answers.some((answer) => answer === CHOSEN.name) && setIsFinished(true);
+      setAnswers(answers);
+    }
   }, []);
 
   const chosenCharacter = characters.find((character) => character.name === CHOSEN.name)!;
@@ -162,12 +167,13 @@ export default function Hiraishindle() {
   };
 
   return (
-    <main className="m-2 flex items-center flex-col font-medium px-3 pb-6">
-      <div className="w-full sm:max-w-[400px]">
-        <form
-          onSubmit={handleSubmitForm}
-          className="flex mb-6 gap-2 relative w-full m-auto items-center justify-center"
-        >
+    <main className="my-2 flex w-full sm:max-w-[400px] justify-center mx-auto items-center flex-col font-medium px-3 pb-6">
+      <form
+        onSubmit={handleSubmitForm}
+        className="mb-6 text-center gap-2 relative w-full m-auto"
+      >
+        <span className="text-lg mb-2 block">Adivinhe o Hokage de hoje!</span>
+        <div className="flex gap-3">
           <Input
             onClick={(e) => e.currentTarget.value && setIsSelectOpen(true)}
             disabled={isFishined}
@@ -177,83 +183,82 @@ export default function Hiraishindle() {
           <button type="submit">
             <MdSend className="w-5 h-5" />
           </button>
-          {formattedCharacters.length !== 0 && (
-            <div
-              ref={ref}
-              className={cn(
-                'w-full border top-12 left-0  h-[400px] ',
-                isSelectOpen ? 'absolute' : 'hidden'
-              )}
-            >
-              <ScrollArea className="h-[inherit] bg-gray-800">
-                <div className="h-0.5 w-full bg-ring" />
-                <div className="divide-y divide-gray-700">
-                  {formattedCharacters.map((character) => (
-                    <div
-                      onSelect={() => console.log(character)}
-                      className="px-4 py-3 cursor-pointer hover:bg-gray-700 transition-all duration-75"
-                      key={character.name}
-                      onClick={() => handleSubmitName(character.name)}
-                    >
-                      {character.name}
-                    </div>
-                  ))}
-                </div>
-              </ScrollArea>
-            </div>
-          )}
-        </form>
-        {answers.length !== 0 && (
-          <div className="text-sm w-[100%] sm:w-[160%] sm:-mx-[30%] overflow-x-auto overflow-y-hidden">
-            <div className="w-[160%] sm:w-[unset] flex gap-2 text-center">
-              {properties.map((property) => (
-                <div
-                  className="flex basis-[calc(16.6%-4px)] w-0 items-center justify-center border-b-4 p-2"
-                  key={property}
-                >
-                  <span>{property}</span>
-                </div>
-              ))}
-            </div>
-            <div className="w-[160%] sm:w-[unset] mt-2 flex flex-col gap-2 text-center">
-              {answers.map((guess) => {
-                const properties = Object.values(
-                  characters.find((character) => character.name === guess)!
-                );
-
-                return (
-                  <div className="flex gap-2" key={guess}>
-                    {properties.map((value, index) => {
-                      const chosenCharacterValues = Object.values(chosenCharacter);
-
-                      return (
-                        <div
-                          className={cn(
-                            'px-0.5 basis-[calc(16.6%-4px)] aspect-[0.95] w-0 flex items-center justify-center font-semibold py-4 rounded-lg border-4-black data-[answer=wrong]:bg-red-700 data-[answer=correct]:bg-green-700 data-[answer=semicorrect]:bg-yellow-700',
-                            index === 0 && 'bg-gray-200 text-black'
-                          )}
-                          data-answer={
-                            index !== 0 &&
-                            checkAnswer(value, chosenCharacterValues[index])
-                          }
-                          key={value.toString()}
-                        >
-                          {formatProperty(value, chosenCharacterValues[index])}
-                        </div>
-                      );
-                    })}
-                  </div>
-                );
-              })}
-            </div>
-            {isFishined && (
-              <span id="gg" className="mt-6 block text-center text-4xl">
-                GG!
-              </span>
+        </div>
+        {formattedCharacters.length !== 0 && (
+          <div
+            ref={ref}
+            className={cn(
+              'w-full border top-[88px] left-0  h-[400px] ',
+              isSelectOpen ? 'absolute' : 'hidden'
             )}
+          >
+            <ScrollArea className="h-[inherit] bg-gray-800">
+              <div className="h-0.5 w-full bg-ring" />
+              <div className="divide-y divide-gray-700">
+                {formattedCharacters.map((character) => (
+                  <div
+                    onSelect={() => console.log(character)}
+                    className="px-4 py-3 cursor-pointer hover:bg-gray-700 transition-all duration-75"
+                    key={character.name}
+                    onClick={() => handleSubmitName(character.name)}
+                  >
+                    {character.name}
+                  </div>
+                ))}
+              </div>
+            </ScrollArea>
           </div>
         )}
-      </div>
+      </form>
+      {answers.length !== 0 && (
+        <div className="text-sm w-[100%] sm:w-[160%] sm:-mx-[30%] overflow-x-auto overflow-y-hidden">
+          <div className="w-[160%] sm:w-[unset] flex gap-2 text-center">
+            {properties.map((property) => (
+              <div
+                className="flex basis-[calc(16.6%-4px)] w-0 items-center justify-center border-b-4 p-2"
+                key={property}
+              >
+                <span>{property}</span>
+              </div>
+            ))}
+          </div>
+          <div className="w-[160%] sm:w-[unset] mt-2 flex flex-col gap-2 text-center">
+            {answers.map((guess) => {
+              const properties = Object.values(
+                characters.find((character) => character.name === guess)!
+              );
+
+              return (
+                <div className="flex gap-2" key={guess}>
+                  {properties.map((value, index) => {
+                    const chosenCharacterValues = Object.values(chosenCharacter);
+
+                    return (
+                      <div
+                        className={cn(
+                          'px-0.5 basis-[calc(16.6%-4px)] aspect-[0.95] w-0 flex items-center justify-center font-semibold py-4 rounded-lg border-4-black data-[answer=wrong]:bg-red-700 data-[answer=correct]:bg-green-700 data-[answer=semicorrect]:bg-yellow-700',
+                          index === 0 && 'bg-gray-200 text-black'
+                        )}
+                        data-answer={
+                          index !== 0 && checkAnswer(value, chosenCharacterValues[index])
+                        }
+                        key={value.toString()}
+                      >
+                        {formatProperty(value, chosenCharacterValues[index])}
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+          {isFishined && (
+            <span id="gg" className="mt-6 block text-center text-4xl">
+              GG!
+            </span>
+          )}
+        </div>
+      )}
     </main>
   );
 }
